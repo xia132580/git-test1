552687f67aab64f179e01604beb932aae7872091
xiachen <1227910768@qq.com>
2024-03-22 20:51:56 +0800

    four commit

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -1,6 +1,6 @@
 package test;
 
-public class Test {
+public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git 1! ! !");
         System.out.println("Hello Git 2! ! !");
