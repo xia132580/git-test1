d5a9d363e1dfa60461ef139b2787a5b3e3e9a651
xiachen <1227910768@qq.com>
2024-03-22 20:13:37 +0800

    two commit

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git 1! ! !");
+        System.out.println("Hello Git 2! ! !");
+        System.out.println("Hello Git 3! ! !");
+    }
+}
